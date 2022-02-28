df43a14832fa5082f8bfef195ca19278ef6d6422
Kishor Balgi <kishorbalgi@gmail.com>
2022-02-28 12:17:05 +0530

    CORS test

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const compression = require('compression');
 // CORS:
 app.use(
   cors({
-    origin: 'https://chat-box-app-client.herokuapp.com/',
+    origin: 'https://chat-box-app-client.herokuapp.com',
     credentials: true,
   })
 );
