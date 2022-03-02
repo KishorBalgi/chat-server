fa4d498202275b73087c73f933a8282a08a24977
Kishor Balgi <kishorbalgi@gmail.com>
2022-03-02 16:09:58 +0530

    Added chatlist model and socket and socket auth

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
-  cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] },
+  cors: {
+    origin: [
+      'https://chat-box-app-client.herokuapp.com',
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST'],
+  },
 });
 const dotenv = require('dotenv');
 const mogoose = require('mongoose');
