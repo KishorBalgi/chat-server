225902c6be99c71e5f8ab0e9a43b2260ffd1b2f5
Kishor Balgi <kishorbalgi@gmail.com>
2022-05-16 20:40:10 +0530

    Enabled gmail email send

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,22 +2,22 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   // Transporter:
-  let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-  // G-Mail:
   // let transporter = nodemailer.createTransport({
-  //   service: process.env.EMAIL_HOST_GMAIL,
+  //   host: process.env.EMAIL_HOST,
+  //   port: process.env.EMAIL_PORT,
   //   auth: {
-  //     user: process.env.EMAIL_USER_GMAIL,
-  //     pass: process.env.EMAIL_PASS_GMAIL,
+  //     user: process.env.EMAIL_USER,
+  //     pass: process.env.EMAIL_PASS,
   //   },
   // });
+  // G-Mail:
+  let transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_HOST_GMAIL,
+    auth: {
+      user: process.env.EMAIL_USER_GMAIL,
+      pass: process.env.EMAIL_PASS_GMAIL,
+    },
+  });
   //   Mail options:
   const mailOptions = {
     from: 'Chat App <apps.kishorbalgi@gmail.com',
